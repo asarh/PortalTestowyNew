b8a191bbb356f7dacb6a83aa9568a15e5a5a3380
asarh <msznyder@gmail.com>
2021-06-16 08:12:01 +0200

    Pierwszy model

---
+++ Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PortalR.API.Models;
+
+namespace PortalR.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){ }
+        public DbSet<Value>  Values { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -8,8 +8,10 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using PortalR.API.Data;
 
 namespace PortalR.API
 {
@@ -25,7 +27,8 @@ namespace PortalR.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddDbContext<DataContext>(x => x.UseSqlite("ConnectionString"));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_0);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,7 +43,7 @@ namespace PortalR.API
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.UseHsts();
             app.UseHttpsRedirection();
             app.UseMvc();
 
